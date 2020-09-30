3beb5888e4d4926e68dccaa9620b7cd0b0eddfd5
Slaton <slsp8752@colorado.edu>
2020-09-30 16:06:35 -0600

    Updates

---
+++ precache-manifest.46bf929720ae2d35c6d338e09487edfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d38a39b792adb267d4458deae061c0",
+    "url": "/plastic-os/index.html"
+  },
+  {
+    "revision": "21843dea354efced741a",
+    "url": "/plastic-os/static/css/main.4af90f7a.chunk.css"
+  },
+  {
+    "revision": "296fd8cfbece92d1e2f8",
+    "url": "/plastic-os/static/js/2.56bd891a.chunk.js"
+  },
+  {
+    "revision": "443d2105bbae44cc4ccf3384197799b0",
+    "url": "/plastic-os/static/js/2.56bd891a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21843dea354efced741a",
+    "url": "/plastic-os/static/js/main.d3f44d95.chunk.js"
+  },
+  {
+    "revision": "7a8e04d71073bba891df",
+    "url": "/plastic-os/static/js/runtime-main.f70f9148.js"
+  }
+]);
